bc9a6dc532fa463090beda1b3c84ff52848d9c12
chae young hoon <chaeyh04@gmail.com>
2021-09-20 13:40:01 +0900

    INSERT * UILinker

---
+++ Assets/Script/NonManager/UI/UILinker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UILinker : MonoBehaviour
+{
+    public Image Icon;
+    public Button IconBtn;
+
+    public Button InfoBtn;
+    public Button ItemBtn;
+
+    public Button AllocBtn;
+}
