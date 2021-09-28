db79acdaeec0d10a383f6d7ff7be309d2fad4628
kkulbeol <chaeyh04@gmail.com>
2021-09-29 01:06:50 +0900

    INSERT * Stage Info Linker

--- Assets/Script/Manager/StageManager.cs
+++ Assets/Script/Manager/StageManager.cs
@@ -3,38 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+public struct RewardInfo
+{
+    public Tuple<int, int, Unit> FirstClear;
+    public Tuple<int, int, Unit> ThreeStarClear;
+}
 
 public class StageManager : MonoBehaviour
 {
-    List<Tuple<int, int, Unit>>[] Rewards;
+    public static StageManager Instance { get; private set; } = null;
 
-    Tuple<int, int, Unit> Reword => Rewards[StageInfo.theme_number][StageInfo.stage_number];
+    public List<RewardInfo> RewardInfos;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     private void Start()
     {
-        Rewards = new List<Tuple<int, int, Unit>>[5];
-        Rewards[0] = new List<Tuple<int, int, Unit>>()
-        {
 
-        };
-        Rewards[1] = new List<Tuple<int, int, Unit>>()
-        {
-
-        };
-        Rewards[2] = new List<Tuple<int, int, Unit>>()
-        {
-
-        };
-        Rewards[3] = new List<Tuple<int, int, Unit>>()
-        {
-
-        };
-        Rewards[4] = new List<Tuple<int, int, Unit>>()
-        {
-
-        };
     }
-
-    public (int jem, int money, Unit unit) GetReward() => (Reword.Item1, Reword.Item2, Reword.Item3);
 }
---
+++ Assets/Script/NonManager/UI/StageInfoLinker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StageInfoLinker : MonoBehaviour
+{
+    public RectTransform MobContent;
+    public RectTransform RewardContent;
+
+    private void Start()
+    {
+        
+    }
+}
