d525ea8a2232ddbdb5d06dc1d0f3ca28fe3481e4
daeho0818@naver.com <dlaeogh2004>
2021-09-30 09:52:20 +0900

    웨이브 ㅇ ㅘㄴ성

---
+++ Assets/Script/NonManager/Unit/EnemyUnit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnit : Unit
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void OnAnimChanged()
+    {
+    }
+    public override void OnEndFrameAnim()
+    {
+    }
+    public override void OnHit(Unit taker, float damage)
+    {
+    }
+    public override void OnAttack(Unit taken)
+    {
+        base.OnAttack(taken);
+    }
+}
