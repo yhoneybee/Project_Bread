291dfe3d849a0621b169a82f80274379db8f0629
kkulbeol <chaeyh04@gmail.com>
2021-10-06 14:12:02 +0900

    INSERT * Memo

---
+++ Assets/Script/메모장.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 메모장 : MonoBehaviour
+{
+    //------------------------------------------------
+    // 저장 해야하는 목록
+    // DateTimer
+    // Deck
+    //
+    //
+    //
+    //
+    //
+    //
+}
