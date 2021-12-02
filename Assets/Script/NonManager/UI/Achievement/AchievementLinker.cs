4c9e33ff75490429d8b6e9f95ce98ef3a8e7a60e
daeho0818 <daeho0818@naver.com>
2021-12-02 13:10:37 +0900

    업적 씬 만드는중

--- Assets/Script/NonManager/UI/Achievement/AchievementLinker.cs
+++ Assets/Script/NonManager/UI/Achievement/AchievementLinker.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AchivementLinker : MonoBehaviour
+// 대호 개발, 업적 씬을 총괄하는 클래스
+public class AchievementLinker : MonoBehaviour
 {
     [SerializeField]
     enum AchiveCase
@@ -28,30 +29,16 @@ public class AchivementLinker : MonoBehaviour
 
     [SerializeField] BasicButton[] showing_buttons = new BasicButton[2];
 
-    [System.Serializable]
-    [SerializeField]
-    class AchiveContent
-    {
-        public AchiveCase achive_case;
-        public int achive_index;
+    [SerializeField] Achieve[] achieve;
 
-        public GameObject achive_content;
-        public Button button;
-        public Image button_image;
-        public Sprite base_sprite;
-        public Sprite able_sprite;
-        public Sprite taked_sprite;
-    }
-    [SerializeField] AchiveContent[] achive_contents;
-
-    BasicButton select_side_button;
+    BasicButton selected_side_button;
     Coroutine set_color = null;
     Coroutine set_default_color = null;
 
     private void Start()
     {
         // 첫 번째(Adventure Button) 외의 버튼 비활성화
-        select_side_button = side_buttons[0];
+        selected_side_button = side_buttons[0];
         for (int i = 1; i < side_buttons.Length; i++)
         {
             side_buttons[i].button_image.color = new Color(0.5f, 0.5f, 0.5f);
@@ -63,19 +50,19 @@ public class AchivementLinker : MonoBehaviour
 
     public void PressSideButton(int button_index)
     {
-        if (select_side_button.button_index != button_index)
+        if (selected_side_button.button_index != button_index)
         {
             if (set_default_color != null) StopCoroutine(set_default_color);
-            set_default_color = StartCoroutine(FadeImage(select_side_button.button_image, new Color(0.5f, 0.5f, 0.5f), true));
+            set_default_color = StartCoroutine(FadeImage(selected_side_button.button_image, new Color(0.5f, 0.5f, 0.5f), true));
 
             for (int i = 0; i < side_buttons.Length; i++)
             {
                 if (side_buttons[i].button_index == button_index)
-                    select_side_button = side_buttons[i];
+                    selected_side_button = side_buttons[i];
             }
 
             if (set_color != null) StopCoroutine(set_color);
-            StartCoroutine(FadeImage(select_side_button.button_image, Color.white));
+            StartCoroutine(FadeImage(selected_side_button.button_image, Color.white));
         }
     }
     public void PressShowingButton(int button_index)
