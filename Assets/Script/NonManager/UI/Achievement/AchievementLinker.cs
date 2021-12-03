b0c294ebc508f6192625d747da2c148367a76155
daeho0818 <daeho0818@naver.com>
2021-12-03 15:20:44 +0900

    업적 씬 대충 배치는 끝난듯

--- Assets/Script/NonManager/UI/Achievement/AchievementLinker.cs
+++ Assets/Script/NonManager/UI/Achievement/AchievementLinker.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 // 대호 개발, 업적 씬을 총괄하는 클래스
 public class AchievementLinker : MonoBehaviour
 {
+    [SerializeField] RectTransform adventure_parent;
+    [SerializeField] RectTransform content_parent;
+    [SerializeField] RectTransform system_parent;
+    [SerializeField] RectTransform character_parent;
 
     private void Start()
     {
