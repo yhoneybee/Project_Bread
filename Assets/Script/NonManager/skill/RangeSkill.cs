490f111b49a02cd9b21585a36e5bd86159ad1a43
chae young hoon <chaeyh04@gmail.com>
2021-12-14 07:02:43 +0900

    range

---
+++ Assets/Script/NonManager/skill/RangeSkill.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeSkill : BaseSkill
+{
+    public float range;
+    public float durationTime;
+    public override void Cast()
+    {
+    }
+}
