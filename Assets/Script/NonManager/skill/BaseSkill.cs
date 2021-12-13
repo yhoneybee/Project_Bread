4a3ff478dc7004c7f8ba6b227dfc38defeda6ad6
yhoney <chaeyh04@gmail.com>
2021-12-13 20:37:13 +0900

    BaseSkill virtual -> abstract

--- Assets/Script/NonManager/skill/BaseSkill.cs
+++ Assets/Script/NonManager/skill/BaseSkill.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseSkill : MonoBehaviour
+public abstract class BaseSkill : MonoBehaviour
 {
     public Unit owner;
     public Sprite skillIcon;
     public float coolTime;
 
-    public virtual void Cast()
-    {
-
-    }
+    public abstract void Cast();
 }
